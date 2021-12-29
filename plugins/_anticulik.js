d14ed4d2258902362e561136e83fa7350b155694
MitsuriBotz <96665638+JulianKastara09@users.noreply.github.com>
2021-12-30 01:19:46 +0700

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -2,16 +2,15 @@ let handler = m => handler.all = async function (m, { isAdmin, isBotAdmin }) {
   if (m.mtype == 'groupInviteMessage') {
 idnya = await this.reply(m.chat, `「 Donasi • Pulsa 」
 
-• *XL:* 6287834993722
-• *DANA:* 6287834993722
+• *IM3:* 6285742344873
+• *DANA:* 6285742344873
 
 Donasi semampunya untuk mendukung bot ini tetap hidup untuk kalian^_^
 Kalo Gak Mau Subscribe Aja Channel Zero 
-https://youtube.com/channel/UC7SydwUESoyOQ3qZZuoaNHw
 
 Kontak Owner Saya⬇️⬇️
 `, m)
-this.sendContact(m.chat, '6287834993722', 'RakuChan', idnya)
+this.sendContact(m.chat, '6285742344873', 'Julian Kastara', idnya)
   }
   return true
 }
