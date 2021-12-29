0c33cac37fd16b8f6952a9a86784bae27054aa46
MitsuriBotz <96665638+JulianKastara09@users.noreply.github.com>
2021-12-30 01:25:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://bit.ly/36cLg9x']
-global.channelYT = ['https://bit.ly/3hg3bm4'] 
-global.owner = ['6287834993722', '62852422542271', '62878349937221']// Put your number here
+global.linkGC = ['https://chat.whatsapp.com/JMA6U58Pxm7JJJUFiAqIOm']
+global.channelYT = ['https://youtube.com/channel/UC6RG7BzAxnZDx-2LUUVVjOg'] 
+global.owner = ['6285742344873']// Put your number here
 global.mods = ['6287834993722', '62852422542271', '62878349937221'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -33,8 +33,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Raku Gans'
-global.author = 'BakaBOTZ Is The Best'
+global.packname = 'Makasih'
+global.author = 'Ngab'
 
 global.multiplier = 69 // The higher, The harder levelup
 
